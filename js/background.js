bc2b60f6ef59a545e708c8d377c076d84a653651
web.ljg <web.ljg@gmail.com>
2022-07-15 16:32:42 +0900

    fix: 백그라운드 이미지 경로 production 경로로 수정

--- js/background.js
+++ js/background.js
@@ -1,9 +1,8 @@
 const images = ['0.jpg', '1.jpg'];
 const screen = document.querySelector('body');
-
 const imagePicker = () => {
   const imagePick = images[Math.floor(Math.random() * images.length)];
-  screen.style.background = `url('../img/${imagePick}') no-repeat center / cover`;
+  screen.style.background = `url('https://dingunote.github.io/vanillajs-challenge/img/${imagePick}') no-repeat center / cover`;
 };
 
 imagePicker();
