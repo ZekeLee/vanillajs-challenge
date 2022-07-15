a738f5878a929a82a928cf9b5e84953c515e6690
web.ljg <web.ljg@gmail.com>
2022-07-15 16:58:51 +0900

    feat: 날씨 아이콘 추가, 날씨 텍스트 삭제

--- js/weather.js
+++ js/weather.js
@@ -4,14 +4,18 @@ const onGeoSuccess = (position) => {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const iconUrl = 'http://openweathermap.org/img/wn/';
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      console.log(data.weather[0].icon);
       const $city = document.getElementById('city');
       const $weather = document.getElementById('weather');
+      const weatherIcon = data.weather[0].icon;
+
       $city.innerText = `${data.name}, ${data.sys.country}`;
-      $weather.innerText = `${data.weather[0].main}, ${data.main.temp}℃`;
+      $weather.innerHTML = `<img src='${iconUrl}${weatherIcon}@2x.png' alt='${data.weather[0].main}'>, ${data.main.temp}℃`;
     });
 };
 const onGeoError = () => {
